9eb52fb337caa862f346c7b2fe2c438bf629de65
Santiago Campos <cam22018@byui.edu>
2023-06-14 13:34:43 -0600

    update again

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app=express();
 
 const port = 443;
 
-const redisClient = Redis.createClient({url:'redis://127.0.0;1:6379'});
+const redisClient = Redis.createClient({url:'redis://127.0.0.1:6379'});
 
 app.use(bodyParser.json()); //allow JSON (Javascript Object Notation) requests
 
